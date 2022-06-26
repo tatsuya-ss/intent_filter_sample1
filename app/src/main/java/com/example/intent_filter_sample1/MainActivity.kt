a7a6d903f60440d306731176b9b95fd2aabcd242
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2022-06-26 10:36:17 +0900

    MyPageActivityへ遷移実装

--- app/src/main/java/com/example/intent_filter_sample1/MainActivity.kt
+++ app/src/main/java/com/example/intent_filter_sample1/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.intent_filter_sample1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.intent_filter_sample1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBinding()
+        setupMyPageButton()
+    }
+
+    private fun setupBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    private fun setupMyPageButton() {
+        binding.myPageButton.setOnClickListener {
+            val intent = MyPageActivity.createIntent(this)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/intent_filter_sample1/MyPageActivity.kt
+++ app/src/main/java/com/example/intent_filter_sample1/MyPageActivity.kt
@@ -1,5 +1,7 @@
 package com.example.intent_filter_sample1
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,11 @@ class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+    }
+
+    companion object {
+        fun createIntent(context: Context): Intent {
+            return Intent(context, MyPageActivity::class.java)
+        }
     }
 }
